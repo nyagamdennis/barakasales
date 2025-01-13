ed14449fa0ae900750040616f0c34d6a97583fe2
dennismnyaga <murildennis7@gmail.com>
2025-01-13 15:13:36 +0300

    changed the allowed hos

--- BarakaProject/settings.py
+++ BarakaProject/settings.py
@@ -146,7 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'https://barakagaspoint.co.ke'
+    'https://barakagaspoint.co.ke',
+    'https://baraka-sales.vercel.app'
 ]
 
 
