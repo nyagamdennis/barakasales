c00ae526a06adb8aa828b89b0ccc75751348af2c
dennismnyaga <dennisnyaga042@gmail.com>
2025-05-05 18:46:30 +0300

    payment plans

--- BarakaProject/urls.py
+++ BarakaProject/urls.py
@@ -32,4 +32,4 @@ if settings.DEBUG:
     
     
 if settings.DEBUG:
-    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
